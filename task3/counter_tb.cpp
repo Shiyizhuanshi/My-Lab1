4f03ac65adbfcfc86a8813be8862a981dbf3c4cc
Han <hj1021@ic.ac.uk>
2022-10-25 23:49:55 +0100

    First_commit

---
+++ task3/counter_tb.cpp
@@ -0,0 +1,58 @@
+#include "Vcounter.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+
+int main(int argc, char **argv, char **env){
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+
+    Vcounter* top = new Vcounter;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->open ("counter.vcd");
+    
+    // init vbuddy
+    if (vbdOpen()!=1) return (-1);
+    vbdHeader("Lab 1: Counter");
+
+    top->clk = 1;
+    top->rst = 1;
+    top->en = 0;
+    vbdSetMode(1);
+
+    for (i=0; i<500; i++){
+        for (clk=0; clk<2; clk++){
+            tfp->dump (2*i+clk);
+            top->clk = !top->clk;
+            top->eval ();
+        }
+
+        // ++++ Send count to Vbuddy
+        // vbdHex(4, (int(top->count) >> 16) & 0xF);
+        // vbdHex(3, (int(top->count) >> 8) & 0xF);
+        // vbdHex(2, (int(top->count) >> 4) & 0xF);
+        // vbdHex(1, int(top->count) & 0xF);
+
+        vbdPlot(int(top->count), 0, 255);
+        vbdCycle(i+1);
+        // end of Vbuddy section
+
+        //change input simuli
+        // top->rst = (i<2) | (i==15);
+
+        top->en = 1;
+        top->rst = vbdFlag();
+        top->vbdVal = vbdValue();
+        if (Verilated::gotFinish()) exit(0);
+    }
+
+    vbdClose();
+    tfp->close();
+    exit(0);
+}
